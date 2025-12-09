f5ad219e5aaa1d44f3e50b0497c3c1d19c3e1df8
hermesdev <hermesdev0131@gmail.com>
2025-12-09 16:16:17 -0300

    fix: add console log

--- static/src/js/mp_overlay.js
+++ static/src/js/mp_overlay.js
@@ -2,6 +2,7 @@
 
 import { Component } from "@odoo/owl";
 
+console.log("MP Overlay Loaded (Odoo 18)");
 export class MPOverlay extends Component {}
 
 MPOverlay.template = "pos_mercadopago_qr.MPOverlay";
