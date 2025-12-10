e9c15686f0ca16350c5d330246a8cb922c71f81d
hermesdev <hermesdev0131@gmail.com>
2025-12-09 21:41:52 -0300

    fix: test #1

--- __manifest__.py
+++ __manifest__.py
@@ -12,16 +12,17 @@
         'views/pos_payment_method_view.xml',
     ],
     'assets': {
-        # 'point_of_sale._assets_pos': [
-        #     'pos_mercadopago_qr/static/src/js/PaymentMP.js',
-        #     'pos_mercadopago_qr/static/src/xml/PaymentMP.xml',
-        # ],
-        "point_of_sale._assets_pos": [
-            'pos_mercadopago_qr/static/src/js/mp_qr_popup.js',
-            'pos_mercadopago_qr/static/src/xml/mp_qr_popup.xml',
-            "pos_mercadopago_qr/static/src/js/payment_mp.js",
-            # "pos_mercadopago_qr/static/src/xml/mp_overlay.xml",
+        'point_of_sale._assets_pos': [
+            'pos_mercadopago_qr/static/src/js/custombutton.js',
+            'pos_mercadopago_qr/static/src/xml/custombutton.xml',
+            'pos_mercadopago_qr/static/src/js/popup.js',
+            'pos_mercadopago_qr/static/src/xml/popup.xml',
         ],
+        # "point_of_sale._assets_pos": [
+        #     'pos_mercadopago_qr/static/src/js/mp_qr_popup.js',
+        #     'pos_mercadopago_qr/static/src/xml/mp_qr_popup.xml',
+        #     "pos_mercadopago_qr/static/src/js/payment_mp.js",
+        # ],
     },
     'installable': True,
     'application': False,
